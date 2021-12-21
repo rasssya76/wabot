4f7066cda504a35cd1768d671901ddbfdd093d54
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2021-12-21 11:42:06 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ global.tum = fs.readFileSync('./src/Ah5.jpeg')
 global.tum2 = fs.readFileSync('./src/emror.jpeg')
 global.thumb = fs.readFileSync('./src/thumb.jpeg')
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
-global.owner = ['6289681378568', '6283820073017', '6283801499848','85290882526','6285240750713','6288286421519','62877745695315','6281515589573',] // Put your number here
-global.mods = ['6285693120588'] // Want some help?
-global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.owner = ['6281515589573', '43670306128570',] // Put your number here
+global.mods = ['6281515589573'] // Want some help?
+global.prems = ['43670306128570','6285893120588',] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -33,7 +33,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'LitRHap'
+global.author = 'R-BOT'
 global.multiplier = 69 // The higher, The harder levelup
 
 let chalk = require('chalk')
